007728a204db76250015fe6dd3d9c4aec9a7f76b
Joe Ellingworth <joeellingworth@Joes-MBP-2.lan>
2024-09-25 11:39:31 +0100

    fname is required

--- test_user.py
+++ test_user.py
@@ -3,9 +3,12 @@ import pytest
 
 def test_an_create_new_user():
     testuser = User("Joe", "Elling", "jelling@gmail.com")
-
     assert testuser.fname == "Joe"
     assert testuser.lname == "Elling"
     assert testuser.email == "jelling@gmail.com"
 
+def test_fname_and_email_are_not_empty():
+    with pytest.raises(ValueError, match="First name is required"):
+        user1 = User("", "Elling", "jelling@gmail.com")
 
+        
--- user.py
+++ user.py
@@ -4,11 +4,14 @@ class User:
     __email = ""
 
     def __init__(self, fname, lname, email):
-        self.fname = fname
-        self.lname = lname 
-        self.email = email
+
+        if fname == "":
+            raise ValueError ("First name is required")
+        else:
+            self.fname = fname
+            self.lname = lname 
+            self.email = email
 
 
 ronnie = User("ron", "Elling", "ronnie@woof.com")
-
 print(ronnie.fname)
