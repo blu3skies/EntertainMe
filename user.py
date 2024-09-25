dfa8ec4d9a52704ebbf35f5f34baa1b2925ade4e
Joe Ellingworth <joeellingworth@Joes-MBP-2.lan>
2024-09-25 12:04:11 +0100

    added valid email checks

--- user.py
+++ user.py
@@ -5,13 +5,19 @@ class User:
 
     def __init__(self, fname, lname, email):
 
-        if fname == "" or email == "":
+        if fname == "":
             raise ValueError ("First name is required")
+        elif email == "":
+            raise ValueError ("Email address is required")
+        elif not self.valid_email_check(email):
+            raise ValueError ("A valid email is required")
         else:
             self.fname = fname
             self.lname = lname 
             self.email = email
+    
+    def valid_email_check(self, email):
+        return '@' in email and '.' in email
 
 
-ronnie = User("ron", "Elling", "ronnie@woof.com")
-print(ronnie.fname)
+#ronnie = User("ron", "Elling", "ronniewoof.com")
