c9469179dacb3f8f5c18ed564a93be21a875b7b1
Joe Ellingworth <joeellingworth@Joes-MBP-2.lan>
2024-09-25 11:13:57 +0100

    create user test written

---
+++ test_user.py
@@ -0,0 +1,11 @@
+from user import User
+import pytest
+
+def an_create_new_user():
+    testuser = User("Joe" "Elling", "jelling@gmail.com")
+
+    assert testuser.fname == "Joe"
+    assert testuser.lname == "Elling"
+    assert testuser.email == "jelling@gmail.com"
+
+
